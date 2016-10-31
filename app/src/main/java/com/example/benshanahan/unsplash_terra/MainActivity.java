e5541f6ea79ebb57b937c00208b277b50629b1fa
Ben <bnshanahan@gmail.com>
2016-10-31 12:24:49 +0000

    Please just work

---
+++ app/src/main/java/com/example/benshanahan/unsplash_terra/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.benshanahan.unsplash_terra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testUpload()
+    {
+        int i = 1+1;
+        //commit for changes
+    }
+}
